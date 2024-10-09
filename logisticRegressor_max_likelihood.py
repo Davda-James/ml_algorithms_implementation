a0e9cd5287a299f89d168995efccd8f827099623
DavdaJames <jrdavda007@gmail.com>
2024-10-09 11:42:54 +0530

    added the logistic regression that uses maximum likelihood and cross entropy concept for loss function and tries to minimze the cross entropy using gradient descent approach

--- logisticRegressor_max_likelihood.py
+++ logisticRegressor_max_likelihood.py
@@ -2,6 +2,7 @@ import numpy  as np
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split as tts 
 from sklearn.metrics import r2_score,accuracy_score
+from sklearn.linear_model import LogisticRegression
 import matplotlib.pyplot as plt 
 class logisticRegressor:
     def __init__(self,learning_rate=0.01,epochs=100):
@@ -10,35 +11,25 @@ class logisticRegressor:
         self.intercept_=None 
         self.coeff_=None
         self.weights=None 
-    def __helper_step(self,y):
-        if y>0:
-            return 1
-        return 0
     def __helper_sigmoid(self,y):
         return 1/(1+np.exp(-y))
     def train(self,xtrain,ytrain):
         xtrain_new=np.insert(xtrain,0,1,axis=1)
-        self.weights=np.ones(xtrain_new.shape[1])
+        self.weights=np.zeros(xtrain_new.shape[1])
         for i in range(self.epochs):    
-            index=np.random.randint(0,xtrain_new.shape[0])
-            y_hat=np.dot(xtrain_new[index,],self.weights)
-            # y_hat_pred_step=self.__helper_step(y_hat)
+            y_hat=np.dot(xtrain_new,self.weights)
             y_hat_pred_sigmoid=self.__helper_sigmoid(y_hat)
-            self.weights=self.weights+(ytrain[index]-y_hat_pred_sigmoid)*self.eta*xtrain_new[index]
-        self.intercept_=self.weights[0]
+            self.weights+=self.eta*(np.dot(ytrain-y_hat_pred_sigmoid,xtrain_new)/xtrain.shape[0])
+            self.intercept_=self.weights[0]
         self.coeff_ =self.weights[1:]
 
-    def predict(self,xtest):
-        return np.where(np.dot(xtest,self.coeff_)+self.intercept_>0,1,0)
-        
 if __name__=="__main__":
-    x,y=make_classification(n_samples=300,n_features=2,n_informative=1,n_redundant=0,n_classes=2,n_clusters_per_class=1,class_sep=5,random_state=80)
-    xtrain,xtest,ytrain,ytest=tts(x,y,random_state=2,test_size=0.3)
-    lr=logisticRegressor(epochs=1000,learning_rate=0.2)
-    lr.train(xtrain,ytrain)
-    y_predicted=lr.predict(xtest)
-    score=accuracy_score(ytest,y_predicted)
-    print(score)
+    x,y=make_classification(n_samples=300,n_features=2,n_informative=1,n_redundant=0,n_classes=2,n_clusters_per_class=1,class_sep=0.9,random_state=80)
+    xtrain,xtest,ytrain,ytest=tts(x,y,random_state=2,test_size=0.2)
+    lr=logisticRegressor(epochs=2500,learning_rate=0.014)
+    lr.train(x,y)
+    lrsk=LogisticRegression(solver='sag',penalty=None)
+    lrsk.fit(x,y)
     plt.figure(figsize=(10,6))
     plt.xlim(x[:, 0].min() - 1, x[:, 0].max() + 1)  # Add padding around the min and max values
     plt.ylim(x[:, 1].min() - 1, x[:, 1].max() + 1)
@@ -49,5 +40,17 @@ if __name__=="__main__":
     m=-lr.coeff_[0]/lr.coeff_[1]
     c=-lr.intercept_/lr.coeff_[1]
     plt.plot(xpoints,m*xpoints+c,color='red')
+    lrm=-(lrsk.coef_[0][0]/lrsk.coef_[0][1])
+    lrc=-(lrsk.intercept_)/lrsk.coef_[0][1]
+    plt.plot(xpoints,lrm*xpoints+lrc,color='magenta')
     plt.tight_layout()
     plt.show()
+    print(lrsk.coef_)
+    print(lrsk.intercept_)
+    print(lr.coeff_)
+    print(lr.intercept_)
+
+    # updation in the w will be done as W= W + eta*1/n*(Y-Y_hat)*X
+    #  there is no requirement of prediction here hence I use the whole data as training data 
+    
+    
